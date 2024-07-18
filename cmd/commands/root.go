f6f162a0252867785e6253c5348161af3f56bf2e
KarimovKamil <imanymuch@gmail.com>
2024-07-18 11:20:21 +0300

    Add main logic for anti brute force app (#1)
    * Add main logic for anti brute force app

---
+++ cmd/commands/root.go
@@ -0,0 +1,22 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "abf",
+	Short: "Anti-brute-force cli tool",
+	Run: func(cmd *cobra.Command, _ []string) {
+		fmt.Println(cmd.UsageString())
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
